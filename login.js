4c99fcee4187634b4714c60839f6955d34842d86
unknown <juliannite678@gmail.com>
2023-01-17 01:48:12 +0800

    Changed cookie handling scripts, added CS course page (still placeholders)

--- login.js
+++ login.js
@@ -1,9 +1,9 @@
 const loginValue = document.cookie.match('(^|;)\\s*' + "login" + '\\s*=\\s*([^;]+)')?.pop() || ''
 
-if (!loginValue) window.location.href = "./login.html";
+if (!loginValue) window.location.href = "/login.html";
 
 const logout = () => {
-    document.cookie = "login= ;";
+    document.cookie = "login=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
 
-    window.location.href = "./login.html"
+    window.location.href = "/login.html"
 }
