cd908501599237223b2f669847a82e606e068577
unknown <juliannite678@gmail.com>
2023-01-17 01:58:47 +0800

    ayoko na

--- login.js
+++ login.js
@@ -1,6 +1,12 @@
 const loginValue = document.cookie.match('(^|;)\\s*' + "login" + '\\s*=\\s*([^;]+)')?.pop() || ''
 
-if (!loginValue) window.location.href = "/login.html";
+if (!loginValue) {
+    if (window.location.href.includes("courses/cs")) {
+        window.location.href = "../../login.html"
+    } else {
+        window.location.href = "./login.html"
+    }
+}
 
 const logout = () => {
     document.cookie = "login=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
