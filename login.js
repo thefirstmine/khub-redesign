3fc56c5124b559d490efe0700a5b7f19269e901f
unknown <juliannite678@gmail.com>
2023-01-17 02:15:14 +0800

    fix i guess

--- login.js
+++ login.js
@@ -2,9 +2,9 @@ const loginValue = document.cookie.match('(^|;)\\s*' + "login" + '\\s*=\\s*([^;]
 
 if (!loginValue) {
     if (window.location.href.includes("courses/cs")) {
-        window.location.href = "../../login.html"
+        window.location.replace("../../login.html")
     } else {
-        window.location.href = "./login.html"
+        window.location.replace("./login.html")
     }
 }
 
@@ -12,8 +12,8 @@ const logout = () => {
     document.cookie = "login=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
 
     if (window.location.href.includes("courses/cs")) {
-        window.location.href = "../../login.html"
+        window.location.replace("../../login.html")
     } else {
-        window.location.href = "./login.html"
+        window.location.replace("./login.html")
     }
 }
