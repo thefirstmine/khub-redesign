c41510ce5e00bf8aeaaabf6b9b82f9e486c01b9f
unknown <juliannite678@gmail.com>
2023-01-17 01:56:28 +0800

    me when 1 more thing broke

--- login.js
+++ login.js
@@ -5,9 +5,9 @@ if (!loginValue) window.location.href = "/login.html";
 const logout = () => {
     document.cookie = "login=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
 
-    if (window.location.href === "courses/cs/index.html") {
+    if (window.location.href.includes("courses/cs")) {
         window.location.href = "../../login.html"
     } else {
-        window.location.href = "/login.html"
+        window.location.href = "./login.html"
     }
 }
